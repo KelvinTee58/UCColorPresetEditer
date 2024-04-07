c1afe81d9d194b2b60d2442e7c088c91cc995a89
郑凯杰 <zhengkaijie@ghbank.com.cn>
2024-04-07 12:51:28 +0800

    stage

--- v3/src/main.ts
+++ v3/src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// 自定义样式
+import "@/assets/styles/index.scss";
+import "./style.css";
+
+const app = createApp(App);
+
+// 路由
+import router from "@/router/index.ts";
+app.use(router);
+
+app.mount("#app");
--- v3/vite.config.ts
+++ v3/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 import path from "path";
@@ -6,17 +6,47 @@ import path from "path";
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
+// svg
+import svgSprites from "rollup-plugin-svg-sprites";
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
+export default defineConfig(({ mode }) => {
+  // 获取`.env`环境配置文件
+  const env = loadEnv(mode, process.cwd());
+
+  console.log(">>>>>>>>> start " + env.VITE_ENV + " env <<<<<<<<<");
+  return {
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
+      },
     },
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+      vue(),
+      svgSprites({
+        exclude: ["node_modules/**"],
+        vueComponent: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    // 反向代理解决跨域问题
+    server: {
+      // host: 'localhost', // 只能本地访问
+      host: "0.0.0.0", // 局域网别人也可访问
+      port: Number(env.VITE_PORT),
+      // 运行时自动打开浏览器
+      // open: env.VITE_OPEN,
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_APP_SERVICE_API,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp("^" + env.VITE_APP_BASE_API), ""),
+        },
+      },
+    },
+  };
 });
