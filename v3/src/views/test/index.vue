4b8dc0c697bbe227527cbca53e25ee818606189a
kelvintee <1440531680@qq.com>
2024-04-12 11:23:36 +0800

    fix(api): 请求api结构和实例

--- v3/src/views/test/index.vue
+++ v3/src/views/test/index.vue
@@ -4,6 +4,7 @@
   <h1>test{{ count }}test</h1>
   <button @click="increment">add</button>
   <button @click="$store.test.useCounterStore().increment">add2</button>
+  <button @click="testApi">testapi</button>
   <h1>test</h1>
   <h1>test</h1>
   <h1>test</h1>
@@ -29,13 +30,19 @@
 </template>
 
 <script setup>
-import { getCurrentInstance } from "vue";
+import { getCurrentInstance,ref } from "vue";
 import { storeToRefs } from "pinia";
 const { proxy } = getCurrentInstance();
 let store = proxy.$store.test.useCounterStore();
 console.log("store", store);
 let { count } = storeToRefs(store);
 let {increment} = store
+async function testApi(){
+  let res = ref(null);
+  // console.log("api", proxy.$api.test1);
+  res = proxy.$api.test1.time();
+}
+
 console.log("count", count);
 </script>
 <style lang="scss" scoped>
